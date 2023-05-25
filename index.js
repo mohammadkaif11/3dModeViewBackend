1c6eca6bce98e591deb642cdb38446e8260533ca
mohammadkaif <83584426+mohammadkaif11@users.noreply.github.com>
2023-05-25 12:18:38 +0530

    adding headers with request headers

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-const express=require('express');
-const connectToMongo=require('./DataContext/Database')
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const connectToMongo = require("./DataContext/Database");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 const app = express();
-app.use(cors())
-app.use(express.static('upload/image'))
-app.use(express.static('upload/3dModels'))
+app.use(cors());
+app.use(express.static("upload/image"));
+app.use(express.static("upload/3dModels"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(fileUpload())
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+//read static files from server
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 connectToMongo();
 
-app.use('/item', require('./Controller/Item'));
+app.use("/item", require("./Controller/Item"));
 
-app.get('/', function(req, res){
-    res.send('working on 8080')
-})
+app.get("/", function (req, res) {
+  res.send("working on 8080");
+});
 
-//listen on 
+//listen on
 app.listen(process.env.PORT || 8080, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
