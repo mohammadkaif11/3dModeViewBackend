dae1fce1dc47d1708ae13db6e5c8657900c150be
mohammadkaif <83584426+mohammadkaif11@users.noreply.github.com>
2023-05-25 13:16:54 +0530

    delete response error

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 
 //read static files from server
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// });
 
 connectToMongo();
 
